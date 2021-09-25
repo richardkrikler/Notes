11128c989920c1b8884280d47935e1270e49f703
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-25 22:41:20 +0200

    removed content from object

--- client/Note/Note.php
+++ client/Note/Note.php
@@ -7,20 +7,17 @@ class Note
     private $pk_note_id;
     private $fk_pk_folder_id;
     private $name;
-    private $content;
 
     /**
      * @param int $pk_note_id
      * @param int $fk_pk_folder_id
      * @param string $name
-     * @param string $content
      */
-    public function __construct(int $pk_note_id, int $fk_pk_folder_id, string $name, string $content)
+    public function __construct(int $pk_note_id, int $fk_pk_folder_id, string $name)
     {
         $this->pk_note_id = $pk_note_id;
         $this->fk_pk_folder_id = $fk_pk_folder_id;
         $this->name = $name;
-        $this->content = $content;
     }
 
     /**
@@ -45,13 +42,5 @@ class Note
     public function getName(): string
     {
         return $this->name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent(): string
-    {
-        return $this->content;
     }
 }
