a5b49ab27683cb262b4f6f25bafcc9ae1e5bda03
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-26 08:23:44 +0200

    minify index

--- client/index.php
+++ client/index.php
@@ -2,36 +2,10 @@
 
 namespace RichardKrikler\CodingNotes;
 
-use PDO;
-use PDOException;
-use RichardKrikler\CodingNotes\DB\DB;
-use RichardKrikler\CodingNotes\Folder\Folder\Folder;
-use RichardKrikler\CodingNotes\Folder\Folders\Folders;
-use RichardKrikler\CodingNotes\Template\IndexTemplate\IndexTemplate;
+use RichardKrikler\CodingNotes\DB\FoldersDB;
+use RichardKrikler\CodingNotes\Template\IndexTemplate;
 
-require_once 'Folder/Folder.php';
-require_once 'Folder/Folders.php';
+require_once 'DB/FoldersDB.php';
 require_once 'Template/IndexTemplate.php';
-require_once 'DB.php';
 
-$DB = DB::getDB();
-
-$folders = new Folders();
-try {
-    $stmt = $DB->prepare('SELECT pk_folder_id, name FROM folders');
-    if ($stmt->execute()) {
-        while ($row = $stmt->fetch()) {
-            $folders->addFolder(new Folder((int)$row['pk_folder_id'], $row['name']));
-        }
-    }
-
-    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException  $e) {
-    print('Error: ' . $e);
-    exit();
-}
-
-
-print(IndexTemplate::render($folders->getUnorderedListHTML()));
-
-
+print(IndexTemplate::render(FoldersDB::getFolders()->getUnorderedListHTML()));
