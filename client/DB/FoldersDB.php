80267afcc333958ba5f9def08bcfba143e516f87
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-26 08:23:21 +0200

    Folder/Note Class for DB functions

---
+++ client/DB/FoldersDB.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace RichardKrikler\CodingNotes\DB;
+
+use PDO;
+use PDOException;
+use RichardKrikler\CodingNotes\Folder\Folder;
+use RichardKrikler\CodingNotes\Folder\Folders;
+
+require_once 'DB.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Folder/Folders.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Folder/Folder.php';
+
+class FoldersDB
+{
+    static function getFolders(): Folders
+    {
+        $DB = DB::getDB();
+        $folders = new Folders();
+        try {
+            $stmt = $DB->prepare('SELECT pk_folder_id, name FROM folders');
+            if ($stmt->execute()) {
+                while ($row = $stmt->fetch()) {
+                    $folders->addFolder(new Folder((int)$row['pk_folder_id'], $row['name']));
+                }
+            }
+            $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException  $e) {
+            print('Error: ' . $e);
+            exit();
+        }
+
+        return $folders;
+    }
+
+    static function getFolderFromID($folder_id): Folder
+    {
+        $DB = DB::getDB();
+        try {
+            $stmt = $DB->prepare('SELECT pk_folder_id, name FROM folders WHERE pk_folder_id = :folder_id');
+            $stmt->bindParam(":folder_id", $folder_id, PDO::PARAM_INT);
+            $folder = new Folder();
+            if ($stmt->execute()) {
+                $row = $stmt->fetch();
+                $folder->setPkFolderId($row['pk_folder_id']);
+                $folder->setName($row['name']);
+            }
+            $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $folder;
+        } catch (PDOException  $e) {
+            print('Error: ' . $e);
+            exit();
+        }
+    }
+
+    static function getFolderFromNoteID($note_id): Folder
+    {
+        $DB = DB::getDB();
+        try {
+            $stmt = $DB->prepare('SELECT pk_folder_id, name FROM notes INNER JOIN folders on fk_pk_folder_id = pk_folder_id WHERE pk_note_id = :note_id');
+            $stmt->bindParam(":note_id", $note_id, PDO::PARAM_INT);
+            $folder = new Folder();
+            if ($stmt->execute()) {
+                $row = $stmt->fetch();
+                $folder->setPkFolderId($row['pk_folder_id']);
+                $folder->setName($row['name']);
+            }
+
+            $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $folder;
+        } catch (PDOException  $e) {
+            print('Error: ' . $e);
+            exit();
+        }
+    }
+}
---
+++ client/DB/NotesDB.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RichardKrikler\CodingNotes\DB;
+
+use PDO;
+use PDOException;
+use RichardKrikler\CodingNotes\Note\Note;
+use RichardKrikler\CodingNotes\Note\Notes;
+
+require_once 'DB.php';
+
+class NotesDB
+{
+    static function getNotesFromFolderID($folder_id): Notes
+    {
+        $DB = DB::getDB();
+        try {
+            $stmt = $DB->prepare('SELECT pk_note_id, fk_pk_folder_id, title FROM notes WHERE fk_pk_folder_id = :folder_id');
+            $stmt->bindParam(":folder_id", $folder_id, PDO::PARAM_INT);
+            $notes = new Notes();
+            if ($stmt->execute()) {
+                while ($row = $stmt->fetch()) {
+                    $notes->addNote(new Note((int)$row['pk_note_id'], (int)$row['fk_pk_folder_id'], $row['title']));
+                }
+            }
+
+            $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $notes;
+        } catch (PDOException  $e) {
+            print('Error: ' . $e);
+            exit();
+        }
+    }
+}
