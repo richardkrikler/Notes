eb6be76709feb94201616f949b6166d1556b3390
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-11-08 19:24:03 +0100

    Corrected JS Ft move between files; maring for tables; main site title

--- src/Template/SiteTemplate.php
+++ src/Template/SiteTemplate.php
@@ -19,7 +19,7 @@ class SiteTemplate
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CodingNotes</title>
+    <title>Notes</title>
 
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
--- src/js/noteEditor.js
+++ src/js/noteEditor.js
@@ -92,7 +92,7 @@ shortcut.add('Meta+S', async function () {
 })
 
 shortcut.add('Tab', async function () {
-    contentTextarea.insertText('\t')
+    contentTextarea.insertText('  ')
 }, {
     'target': contentTextarea.element
 })
@@ -100,3 +100,52 @@ shortcut.add('Tab', async function () {
 shortcut.add('Meta+E', async function () {
     await viewer()
 })
+
+async function saveFile(data) {
+    data = data.substr(22)
+
+    const today = new Date()
+    const twoDigitStr = input => (input).toLocaleString(undefined, {minimumIntegerDigits: 2})
+    const dateString = twoDigitStr(today.getDate()) + "-" +
+        twoDigitStr(today.getMonth() + 1) + "-" +
+        today.getFullYear() + "_" +
+        twoDigitStr(today.getHours()) + "-" +
+        twoDigitStr(today.getMinutes()) + "-" +
+        twoDigitStr(today.getSeconds())
+    const name = "image_" + dateString
+
+    await fetch("File/CreateFile.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, data}),
+    }).then(() => {
+        contentTextarea.insertText('![' + name + '](/File/viewer.php?name=' + name + ')')
+    })
+}
+
+
+async function saveFileFromUrl(event, value) {
+    if (event.key !== 'Enter') {
+        return
+    }
+
+    await fetch(value).then(res => res.blob())
+        .then(blob => {
+            const reader = new FileReader()
+            reader.onload = function () {
+                saveFile(this.result)
+            }
+            reader.readAsDataURL(blob)
+        })
+}
+
+async function saveFileFromInput(event) {
+    const selectedFile = event.target.files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(selectedFile)
+    reader.onload = function () {
+        saveFile(reader.result)
+    }
+}
