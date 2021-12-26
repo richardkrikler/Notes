3ce50318859d840708576bddce66695f24646236
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-12-26 23:10:37 +0100

    Settings: Added table of existing Keyboard-Shortcuts

--- src/settings.php
+++ src/settings.php
@@ -32,6 +32,51 @@ print(SiteTemplate::render(new SettingsNav(), <<<SETTINGS
             <label for="dark-mode">Dark Mode</label>
         </div>-->
     </div>
+    <div class="mt-5">
+        <h1>Keyboard-Shortcut List</h1>
+        <table class="table table-striped table-bordered rounded-1 mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Cmd + Option + N</th>
+                    <td>Create new Folder (in index) / Note (in folder)</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + E</th>
+                    <td>Switch between Note Viewer and Editor</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + G</th>
+                    <td>Viewer: Toggle Table-of-Contents Modal</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + Esc</th>
+                    <td>Viewer: clear URL from Header-ID</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + S</th>
+                    <td>Editor: Save Note</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + B</th>
+                    <td>Editor: Bold-Text (insert **)</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + K</th>
+                    <td>Editor: Italic-Text (insert *)</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cmd + Option + C</th>
+                    <td>Editor: Code-Block (insert ''')</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 SETTINGS
 ));
