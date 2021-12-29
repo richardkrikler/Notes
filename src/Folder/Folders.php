05dccecdd7d90f25d932562cdcd764f927aac2d9
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-12-29 15:26:54 +0100

    Folder/Note-Boxes: link spans around complete box

--- src/Folder/Folders.php
+++ src/Folder/Folders.php
@@ -42,9 +42,9 @@ class Folders
     {
         $result = '<div class="container mt-5"><div id="folder-grid" class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4 gap-5 justify-content-center mx-4 pb-5">';
         foreach ($this->folders as $folder) {
-            $result .= '<div class="folder-box col d-flex justify-content-center flex-column rounded">
-                            <a class="text-center text-decoration-none h4 mb-0 px-2 py-2 fw-normal" href="folder/' . $folder->getPkFolderId() . '">' . $folder->getName() . '</a>
-                        </div>';
+            $result .= '<a class="folder-box col d-flex justify-content-center flex-column rounded text-decoration-none" href="folder/' . $folder->getPkFolderId() . '">
+                            <span class="text-center h4 mb-0 px-2 py-2 fw-normal">' . $folder->getName() . '</span>
+                        </a>';
         }
         $result .= '</div></div>';
 
